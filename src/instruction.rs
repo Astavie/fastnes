519f2d72e0188107accce9e5878038dec13fa655
Astavie <astavie@pm.me>
2023-04-01 19:18:08 +0200

    remove instruction arc

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,9 +1,9 @@
 use crate::cpu::CPU;
 use repeated::repeated;
-use std::{fmt::Debug, sync::Arc};
+use std::fmt::Debug;
 
 pub type Instruction = fn(&mut CPU);
-pub type Instructions = Arc<[Instruction; 256]>;
+pub type Instructions = [Instruction; 256];
 
 trait Addressable {
     fn poke(&self, cpu: &mut CPU);
@@ -986,11 +986,11 @@ const fn branch<const MASK: u8, const ZERO: bool>() -> Instruction {
 }
 
 pub fn instructions() -> Instructions {
-    Arc::new(repeated!(
+    repeated!(
         %%s prelude [ prelude s%%
         for op in [0;255] {
             instruction::<%%op%%>(),
         }
         %%e postlude ] postlude e%%
-    ))
+    )
 }
